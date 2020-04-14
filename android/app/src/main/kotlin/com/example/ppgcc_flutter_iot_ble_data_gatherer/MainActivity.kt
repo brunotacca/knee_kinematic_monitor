367e447057c469b5787b0ca390b09dc72aebf176
Bruno Tacca <brunotacca@gmail.com>
2020-04-14 19:52:43 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/ppgcc_flutter_iot_ble_data_gatherer/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ppgcc_flutter_iot_ble_data_gatherer
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
